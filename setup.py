0e1daba5f6687b4ead819560ca0ef3e24b56a853
Gregory Wiltshire <mellon.greg@gmail.com>
2022-11-19 14:07:08 -0500

    cleanup setup

--- setup.py
+++ setup.py
@@ -2,19 +2,9 @@ from setuptools import find_packages, setup
 
 setup(
     name='mwaacli',
-    version='0.0.0',
+    version='0.1.0',
+    author='Gregory Wiltshire',
+    author_email='mellon.greg@gmail.com',
     packages=find_packages(exclude=['tests']),  # Include all the python modules except `tests`.
-    description='My custom package tested with tox',
-    long_description='A long description of my custom package tested with tox',
-    install_requires=[
-        # Additional requirements, or parse the requirements file and add it here
-    ],
-    classifiers=[
-        'Programming Language :: Python',
-    ],
-    # entry_points={
-    #     'pytest11': [
-    #         'tox_tested_package = tox_tested_package.fixtures'
-    #     ]
-    # },
+    description='Python Client for Apache CLI on AWS Managed Airflow',
 )
