8e03027b5ecb58ad4bb931d01fcdb7f69c30756a
Gregory Wiltshire <mellon.greg@gmail.com>
2022-11-22 13:16:51 -0500

    cleanup metadata

--- mwaah/__init__.py
+++ mwaah/__init__.py
@@ -4,11 +4,11 @@ import importlib.metadata
 __title__ = "mwaah"
 m = importlib.metadata.metadata(__title__)
 
-__description__ = m.get('Summary')
-__url__ = m.get('Project-URL')
+__description__ = m.get("Summary")
+__url__ = m.get("Project-URL")
 __version__ = "0.1.1"
 __author__ = "Gregory Wiltshire"
-__author_email__ = m.get('Author-email')
-__license__ = m.get('License')
-__copyright__ = "Copyright 2022 Gregory Wiltshire"
+__author_email__ = m.get("Author-email")
+__license__ = m.get("License")
+__copyright__ = f"Copyright 2022 {__author__}"
 __mwaah__="\U0001F618"
